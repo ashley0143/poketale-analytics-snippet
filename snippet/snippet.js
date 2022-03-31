c84c667c3eb4ba9b663a63b4d3d8c7b81f976d25
Ashley <iamashley@tuta.io>
2022-03-31 12:51:15 +0300

    Update snippet.js

--- snippet/snippet.js
+++ snippet/snippet.js
@@ -1,10 +1,9 @@
 /* 
-    Copyright (C) 2021-2022 Ashley (https://github.com/iamashley0)
+    Copyright (C) 2021-2022 Ashley (https://github.com/iamashley0/poketale-anylitcs-snippet)
    
     This Source Code Form is subject to the terms of the Mozilla Public
    - License, v. 2.0. If a copy of the MPL was not distributed with this file,
    - You can obtain one at http://mozilla.org/MPL/2.0/
-   
    */
 function _dntEnabled(dnt, userAgent) {
    /*
