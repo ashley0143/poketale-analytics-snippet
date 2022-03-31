a6094914500df86503661b09f1435e5671b34f97
Ashley <iamashley@tuta.io>
2022-03-31 12:51:53 +0300

    Update snippet-minified.js

--- snippet/snippet-minified.js
+++ snippet/snippet-minified.js
@@ -1,3 +1,10 @@
+/* 
+    Copyright (C) 2021-2022 Ashley (https://github.com/iamashley0/poketale-anylitcs-snippet)
+   
+    This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this file,
+   - You can obtain one at http://mozilla.org/MPL/2.0/
+*/
 "use strict";function t(t,n){
 /*
    * Returns true or false based on whether doNotTack is enabled. It also takes into account the
